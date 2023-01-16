62c20252be480372379a5352071d7deae7737a54
oliverfind <116543690+oliverfind@users.noreply.github.com>
2023-01-16 09:47:26 +0100

    Add files via upload

---
+++ gangekalkulator/script.js
@@ -0,0 +1,8 @@
+const nummer1 = document.getElementById("nummer1")
+const nummer2 = document.getElementById("nummwe2")
+const btn = document.getElementById("btn")
+const resultat = document.getElementById("resultat")
+
+function myFunction (nummer1, nummer2) {
+    return nummer1 * nummer2;
+}
